a385f9286dab7793bae239044de0fbb5f0d3a1b3
VELMURUGAN CHOKKANATHAN <2433818@Swansea.ac.uk>
2026-03-11 08:44:32 +0000

    UIControllerScriptAdded

---
+++ Assets/Script/SimpleUIController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleUIController : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public GameObject infoPanel;
+    public Button infoButton;
+    public Text buttonText;
+
+    [Header("Settings")]
+    public bool showOnStart = true;
+
+    private bool isInfoVisible;
+
+    void Start()
+    {
+        if (infoButton != null)
+        {
+            infoButton.onClick.AddListener(ToggleInfo);
+        }
+
+        if (showOnStart)
+        {
+            ShowInfo();
+        }
+        else
+        {
+            HideInfo();
+        }
+    }
+
+    public void ToggleInfo()
+    {
+        if (isInfoVisible)
+        {
+            HideInfo();
+        }
+        else
+        {
+            ShowInfo();
+        }
+    }
+
+    public void ShowInfo()
+    {
+        if (infoPanel != null)
+        {
+            infoPanel.SetActive(true);
+        }
+
+        isInfoVisible = true;
+
+        if (buttonText != null)
+        {
+            buttonText.text = "HIDE INFO";
+        }
+    }
+
+    public void HideInfo()
+    {
+        if (infoPanel != null)
+        {
+            infoPanel.SetActive(false);
+        }
+
+        isInfoVisible = false;
+
+        if (buttonText != null)
+        {
+            buttonText.text = "SHOW INFO";
+        }
+    }
+}
